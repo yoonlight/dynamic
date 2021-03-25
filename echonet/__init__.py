dceeb0c7863ef8f73dd2f89d360cef7e4aad2b0a
Bryan He <bryanhe@stanford.edu>
2021-03-24 18:39:24 -0700

    copy video models from https://github.com/pytorch/vision/blob/f7fae490980885e426fef01bb214025b9eddb832/torchvision/models/video/resnet.py

--- echonet/__init__.py
+++ echonet/__init__.py
@@ -7,6 +7,7 @@ prediction models.
 from echonet.__version__ import __version__
 from echonet.config import CONFIG as config
 import echonet.datasets as datasets
+import echonet.models as models
 import echonet.utils as utils
 
-__all__ = ["__version__", "config", "datasets", "utils"]
+__all__ = ["__version__", "config", "datasets", "models", "utils"]
